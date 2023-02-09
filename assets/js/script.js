bbff70b49cc6c17efa3ecab261927faaa0904010
GingeHagler <peterrjames1978@outlook.com>
2023-02-09 13:16:53 +0000

    Add css structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 const cardsContainer = document.querySelector(".cards");
-const cardList = document.querySelectorAll('card');
-const cardCount = cardList.length;
+const cards = ["Anna", "Belle", "Elsa"];
+const cardsPickList = [...cards, ...cards];
+const cardCount = cardsPickList.length;
 
 let revealedCount = 0;
 let activeCard = null;
 let awaitingRndOfMove = false;
 
 for (let i = 0; i < cardCount; i++) {
-    const randomIndex = Math.floor(Math.random())
+    const randomIndex = Math.floor(Math.random() * cardsPickList.length);
+    const card = cardsPickList[randomIndex];
+
+    cardsPickList.splice(randomIndex, 1);
+    
+
+    console.log(cardsPickList);
 }
